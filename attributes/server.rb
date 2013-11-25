27fd315fbf4dcc69417a41c91d0245b40006b874
Steffen Gebert <steffen.gebert@typo3.org>
2013-11-25 22:21:03 +0100

    [site-citypo3org] Use Debian repo to install LTS versions of Jenkins

--- attributes/server.rb
+++ attributes/server.rb
@@ -1,7 +1,3 @@
-node.override[:jenkins][:server][:install_method] = "war"
-node.override[:jenkins][:server][:version] = "1.537"
-node.override[:jenkins][:server][:war_checksum] = "411973e8196a61445eb4ef8284975ef4be638738aa806e39fc7bdb08c6d5c261"
-
 node.override[:jenkins][:server][:host] = "localhost"
 node.override[:jenkins][:server][:port] = 8080
 node.override[:jenkins][:server][:url] = "http://localhost:8080"
--- recipes/default.rb
+++ recipes/default.rb
@@ -5,3 +5,6 @@ end
 include_recipe "jenkins::proxy"
 include_recipe "jenkins::server"
 
+# go for LTS
+resources("apt_repository[jenkins]").uri 'http://pkg.jenkins-ci.org/debian-stable' if node[:platform_family].eql?("debian") && node[:jenkins][:server][:install_method].eql?("package")
+
